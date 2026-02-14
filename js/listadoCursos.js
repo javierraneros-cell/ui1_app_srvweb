3ae3485afd34f6cd7994658d26f28f71abc5b06a
Javier Raneros <javier.raneros@alumnos.ui1.es>
2026-02-14 13:07:24 +0100

    Comenzamos a cargar los dato según los requisitos desde el fichero data/cursos.json. Para ello creamos una función en javascript que mediante fetch() recupera todos los datos, esta función ya está preparada para filtrar por el título y por la categoría. El título podrá ser parecico y la categoría exacta

---
+++ js/listadoCursos.js
@@ -0,0 +1,51 @@
+function cargarCursos(tituloFiltro = "", categoriaFiltro = "") {
+    fetch("data/cursos.json")
+        .then(response => response.json())
+        .then(listadoCursos => {
+
+            const contenedor = document.getElementById("contenedor-cursos");
+
+            //Antes de pintar, limpiamos el contenedor
+            contenedor.innerHTML = "";
+
+            // Normalizamos filtros
+            const tituloBuscar = tituloFiltro.toLowerCase().trim();
+            const categoriaBuscar = categoriaFiltro.trim();
+
+            // Filtramos cursos CATEGORIA exacta, TITULO parcial (si se introduce texto)
+            const cursosFiltrados = listadoCursos.filter(curso => {
+                const coincideTitulo = curso.titulo.toLowerCase().includes(tituloBuscar);
+
+                // Si no se selecciona categoría → no filtra por categoría
+                const coincideCategoria = categoriaBuscar === "" 
+                    ? true 
+                    : curso.categoria === categoriaBuscar;
+
+                return coincideTitulo && coincideCategoria;
+            });
+
+            // Pinta CARDs
+            cursosFiltrados.forEach(curso => {
+                const card = `
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card h-100 shadow-sm d-flex flex-column">
+                            <img src="${curso.imagen}" class="card-img-top" alt="${curso.titulo}">
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title">${curso.titulo}</h5>
+                                <ul class="lista-generica">
+                                    <li><strong>Categoría: </strong>${curso.categoria}</li>
+                                    <li><strong>Nivel: </strong>${curso.nivel}</li>
+                                </ul>
+                                <a href="${curso.detalle}?id=${curso.id}" class="btn btn-outline-primary w-100 mt-auto">
+                                    Ver detalles
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                `;
+
+                contenedor.innerHTML += card;
+            });
+        })
+        .catch(error => console.error("Error cargando cursos:", error));
+}
