0e868df93ec97f2db5fc34cedb7917d9bf218c9d
Javier Raneros <javier.raneros@alumnos.ui1.es>
2026-02-18 20:38:48 +0100

    Centralizado el menu en todas las páginas y su JS

--- js/index.js
+++ js/index.js
@@ -23,27 +23,3 @@ $(document).ready(function () {
         }
     });
 });
-
-/**
- * Este funcion se encarga de leer los cursos del fichero JSON y crear dinamicamente el menu del listado de cursos
- */
-document.addEventListener("DOMContentLoaded", cargarCursosEnMenu);
-
-function cargarCursosEnMenu() {
-    fetch("data/cursos.json")
-        .then(response => response.json())
-        .then(listadoCursos => {
-            const lista = document.getElementById("listaCursos");
-
-            listadoCursos.forEach(curso => {
-                const li = document.createElement("li");
-                li.innerHTML = `
-                    <a class="dropdown-item" href="detalle-curso.html?id=${curso.id}">
-                        ${curso.titulo}
-                    </a>
-                `;
-                lista.appendChild(li);
-            });
-        })
-        .catch(err => console.error("Error cargando cursos:", err));
-}
---
+++ js/menu.js
@@ -0,0 +1,36 @@
+/**
+ * Este funcion se encarga de leer los cursos del fichero JSON y crear dinamicamente el menu del listado de cursos
+ */
+function cargarCursosEnMenu() {
+    fetch("data/cursos.json")
+        .then(response => response.json())
+        .then(listadoCursos => {
+            const lista = document.getElementById("listaCursos");
+
+            listadoCursos.forEach(curso => {
+                const li = document.createElement("li");
+                li.innerHTML = `
+                    <a class="dropdown-item" href="detalle-curso.html?id=${curso.id}">
+                        ${curso.titulo}
+                    </a>
+                `;
+                lista.appendChild(li);
+            });
+        })
+        .catch(err => console.error("Error cargando cursos:", err));
+}
+
+/**
+ * Cargamos el menu.html que es comun a todas las paginas
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("menu.html")
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById("contenedor-menu").innerHTML = html;
+            cargarCursosEnMenu();
+            //setTimeout(() => { cargarCursosEnMenu(); }, 0)
+        });
+});
+
+
