ca36b92db2c6ebffcd085566135e7bf8be624307
Javier Raneros <javier.raneros@alumnos.ui1.es>
2026-02-18 21:47:46 +0100

    Añadido el footer a todas las páginas de forma centralizada y con las peticiones que había hecho de la hora.

---
+++ js/footer.js
@@ -0,0 +1,25 @@
+/**
+ * Cargamos el footer.html que es comun a todas las paginas
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("footer.html")
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById("contenedor-footer").innerHTML = html;
+        });
+
+    //Actualización automática cada 1 segundo y muestra de la hora:
+    setInterval(actualizarReloj, 1000);
+
+    // Primera ejecución inmediata
+   // actualizarReloj();
+});
+
+
+function actualizarReloj() {
+    const ahora = new Date();
+    const horas   = String(ahora.getHours()).padStart(2, "0");
+    const minutos = String(ahora.getMinutes()).padStart(2, "0");
+    const segundos = String(ahora.getSeconds()).padStart(2, "0");
+    document.getElementById("contenedor-hora").textContent = `${horas}:${minutos}:${segundos}`;
+}
